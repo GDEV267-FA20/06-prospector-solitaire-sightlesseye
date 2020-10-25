2f3d908572b911aaca69fdfd1c8c17103983283b
Ryan Canuel <ryancanuel@hotmail.com>
2020-10-24 20:03:46 -0400

    Card prefab is CardProspector

--- Assets/Scripts/Prospector.cs
+++ Assets/Scripts/Prospector.cs
@@ -16,9 +16,12 @@ public class Prospector : MonoBehaviour {
 
     [Header("Set in Inspector")]
     public TextAsset deckXML;
+    public TextAsset layoutXML;
 
     [Header("Set Dynamically")]
     public Deck deck;
+    public Layout layout;
+    public List<CardProspector> drawPile;
 
     void Awake() { 
         S = this; // Set up a Singleton for Prospector
@@ -27,12 +30,26 @@ public class Prospector : MonoBehaviour {
     void Start() {
         deck = GetComponent<Deck>(); // Get the Deck
         deck.InitDeck(deckXML.text); // Pass DeckXML to it
-        Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference // a
+        Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference
 
-        Card c;
-        for (int cNum = 0; cNum < deck.cards.Count; cNum++) { 
-            c = deck.cards[cNum]; 
-            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0); 
-        }
+        //Card c;
+        //for (int cNum=0; cNum<deck.cards.Count; cNum++) {
+        //  c = deck.cards[cNum];
+        //  c.transform.localPosition = new Vector3((cNum%13)*3,cNum/13*4,0);
+        //}
+
+        layout = GetComponent<Layout>();  // Get the Layout component
+        layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD) { 
+        List<CardProspector> lCP = new List<CardProspector>(); 
+        CardProspector tCP; 
+        foreach (Card tCD in lCD) { 
+            tCP = tCD as CardProspector;  
+            lCP.Add(tCP); 
+        } 
+        return (lCP); 
     }
 }
